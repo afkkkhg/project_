0c7333650434224c34585f021a967bf7096140c6
unknown <sofia.k2004@mail.ru>
2022-09-22 13:53:44 +0300

    init commit

---
+++ Lesson 2.py
@@ -0,0 +1,8 @@
+a = '456 7899'
+print(a[::-1])
+
+a = '765 3409'
+b = a.split()
+print(b)
+new_str = str(b[1])+" "+ str(b[0])
+print(new_str)
---
+++ lesson 1.py
@@ -0,0 +1,35 @@
+'''
+i = 3
+while i > 0:
+    preference = input()
+    print('Предпочтение учтено')
+    i -= 1
+else:
+   print('Система рекомендаций настроена!')
+
+
+'''
+
+
+a = 5
+
+string_1 ='привет'
+print(string_1)
+
+print(9/2)
+
+a = 7
+print(a-2)
+
+
+a = 'abc'
+b = 'def'
+print(a*10)
+
+a = 'r'
+print(a*50)
+a = '554666564565656565688'
+print(a[0:9:3])
+
+
+a = '989 767'
